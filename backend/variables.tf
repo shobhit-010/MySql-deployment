c9071c31b6e9cd860c0447514f037d05844f0da2
shobhit_010 <shobhitgoyal010@gmail.com>
2025-11-20 11:36:57 -0500

    Add backend provisioning module

---
+++ backend/variables.tf
@@ -0,0 +1,11 @@
+variable "backend_region" {
+  default = "ap-south-1"
+}
+
+variable "state_bucket_name" {
+  default = "mysql-terraform-state-bucket-shobhit-2025"
+}
+
+variable "lock_table_name" {
+  default = "mysql-terraform-lock-table-shobhit-2025"
+}
